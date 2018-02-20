d0afe382424237dfabd2055b2f17b46a7a5f543d
harishbalasubramanian <harishbala153@gmail.com>
2018-02-19 21:10:42 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,7 @@
+/*Author: Harish B
+2/19/18
+Feel Free to Fork and Extend
+But remember to copy and paste this comment*/
 import javax.swing.JFrame;
 public class fir {
 	public static void main(String[] args){
